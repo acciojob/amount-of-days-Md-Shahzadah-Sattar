fc6815c672a349e1b69556c1f90dc2a2f26687f9
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-15 18:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let isleap = false;
+	if(year % 4 ===0){
+		if(year % 100 ===0){
+			return year % 400 ===0 ? 366 : 365;
+		}
+		return 366;
+	}
+	return 365;
+}
